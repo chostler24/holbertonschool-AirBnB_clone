f2998a4aead7c6294787f51cd668066dd31ea533
chostler24 <connor.hostler@gmail.com>
2022-09-29 12:00:22 -0700

    changed __str__ method, added to_dict method

--- models/base_model.py
+++ models/base_model.py
@@ -6,22 +6,27 @@ from datetime import datetime, timezone
 class BaseModel:
     """AirBnB Base class"""
     id = uuid.uuid4()
-    
+
     def __init__(self, id=None):
         """__init__ of self"""
         if id is not None:
             self.id = str(id)
             self.created_at = datetime.now(timezone.utc)
             self.updated_at = datetime.now(timezone.utc)
-            
+
     def __str__(self):
         """overriding __str__ to print custom string"""
-        print("[{}] {} <{}>".format(__class__, self.id, __dict__))
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__
+        )
 
     def save(self):
         """update update_at to current datetime"""
         self.updated_at = datetime.now(timezone.utc)
-        
+
     def to_dict(self):
         """Returns dictionary of __dict__"""
-        
+        new_dict = self.__dict__.copy()
+        new_dict['__class__'] = self.__class__.__name__
+        new_dict['created_at'] = self.created_at
+        new_dict['updated_at'] = self.updated_at
