8a71fffd6541e81110de01d6358fbb3e50bc5bce
CalebHouse99 <calebhouse99@gmail.com>
2022-09-29 15:52:45 -0700

    test for docs

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -7,4 +7,12 @@ from models.base_model import BaseModel
 class TestBaseModel(unittest.TestCase):
     """Unittest class for Base Model"""
     def test___init__(self):
+        """Testing for init"""
         self.assertNotEqual(self.id, "")
+
+    def test_docs(self):
+        """Testing for documentation"""
+        self.assertNotEqual(BaseModel.__init__.__doc__, "")
+        self.assertNotEqual(BaseModel.__str__.__doc__, "")
+        self.assertNotEqual(BaseModel.save.__doc__, "")
+        self.assertNotEqual(BaseModel.to_dict.__doc__, "")
