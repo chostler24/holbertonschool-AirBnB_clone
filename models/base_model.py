ee8fb66de3557bbeadb6fa2fb02485b3b4995fcd
CalebHouse99 <calebhouse99@gmail.com>
2022-09-30 12:32:58 -0700

    added storage

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Base Model"""
 import uuid
 from datetime import datetime
+from models import storage
 
 
 class BaseModel:
@@ -21,6 +22,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """overriding __str__ to print custom string"""
@@ -31,6 +33,7 @@ class BaseModel:
     def save(self):
         """update update_at to current datetime"""
         self.updated_at = datetime.now()
+        storage.save(self)
 
     def to_dict(self):
         """Returns dictionary of __dict__"""
