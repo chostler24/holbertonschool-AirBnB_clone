485c124dd99c5ebb96ffb6904aeee3965abcaecd
chostler24 <connor.hostler@gmail.com>
2022-09-30 11:30:21 -0700

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,20 @@ from datetime import datetime
 class BaseModel:
     """AirBnB Base class"""
 
-    def __init__(self, id=None):
+    def __init__(self, *args, **kwargs):
         """__init__ of self"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key in kwargs:
+                if key != ("__class__"):
+                    setattr(self, key, kwargs[key])
+                if key in ("updated_at", "created_at"):
+                    kwargs[key] = datetime.strptime(
+                        kwargs[key], '%Y-%m-%dT%H:%M:%S.%f'
+                    )
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """overriding __str__ to print custom string"""
