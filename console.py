98bae8af79153529c653be32b5334df7ec1588bc
chostler24 <connor.hostler@gmail.com>
2022-09-30 13:53:49 -0700

    reverting

--- console.py
+++ console.py
@@ -9,11 +9,11 @@ class HBNBCommand(cmd.Cmd):
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        raise SystemExit
+        return True
 
     def do_EOF(self, arg):
         """EOF command to exit the program"""
-        raise SystemExit
+        return True
 
     def empty_line(self):
         """Empty line + Enter does nothing"""
