4a0f093bb769ab550833f7fcd7998fb4a9039b91
chostler24 <connor.hostler@gmail.com>
2022-09-30 11:35:49 -0700

    task 5

--- models/base_model.py
+++ models/base_model.py
@@ -11,16 +11,16 @@ class BaseModel:
         """__init__ of self"""
         if kwargs:
             for key in kwargs:
-                if key != ("__class__"):
-                    setattr(self, key, kwargs[key])
-                if key in ("updated_at", "created_at"):
+                if key in ('updated_at', 'created_at'):
                     kwargs[key] = datetime.strptime(
                         kwargs[key], '%Y-%m-%dT%H:%M:%S.%f'
                     )
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = datetime.now()
+                if key != ('__class__'):
+                    setattr(self, key, kwargs[key])
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def __str__(self):
         """overriding __str__ to print custom string"""
