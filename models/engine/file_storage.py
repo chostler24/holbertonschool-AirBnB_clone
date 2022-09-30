865a4c8a113b2b09c7d0e749d996d30b4bf1ca11
CalebHouse99 <calebhouse99@gmail.com>
2022-09-30 12:03:44 -0700

    updated save(self)

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,41 @@
 #!/bin/usr/python3
 """File storage"""
 import json
+import sys
+
+
+def save_to_json_file(my_obj, filename):
+    """Write to text file from object"""
+    with open(filename, "w", encoding="utf-8") as file:
+        file.write(json.dumps(my_obj))
+
+
+def load_from_json_file(filename):
+    """Create object from JSON"""
+    with open(filename, 'r') as file:
+        return json.load(file)
+
 
 class FileStorage:
     """File Storage Class"""
     __objects = {}
-    
-    
+    __file_path = "file.json"
+
     def all(self):
         """returns the dictionary __objects"""
         return self.__objects
-    
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
+        from sys import argv
+        try:
+            lst = (load_from_json_file(self.__file_path))
+        except FileNotFoundError:
+            lst = []
+        save_to_json_file(lst + argv[1:], self.__file_path)
+
     def reload(self):
-        """deserializes the JSON file to __objects (only if the JSON file (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist, no exception should be raised)"""
+        """deserializes the JSON file to __objects"""
