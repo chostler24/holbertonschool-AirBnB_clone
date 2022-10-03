b0d3d6a9088644bf9426251659b937fd00e1a1c5
chostler24 <connor.hostler@gmail.com>
2022-10-03 15:39:26 -0700

    fix

--- models/user.py
+++ models/user.py
@@ -5,9 +5,7 @@ from base_model import BaseModel
 
 class User(BaseModel):
     """defines class User inheriting from BaseModel"""
-    def __init__(self):
-        """attributes"""
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
